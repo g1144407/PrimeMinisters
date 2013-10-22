c42dba9a9aceee0ea375b7df293913463c2a9a3d
g1144407 <yuki@yuki-no-MacBook-Air.local>
2013-10-22 14:04:25 +0900

    Tuple スタブ作成

--- PrimeMinistersByJava/primeministers/Tuple.java
+++ PrimeMinistersByJava/primeministers/Tuple.java
@@ -1,5 +1,8 @@
 package primeministers;
 
+import java.util.ArrayList;
+
+
 /**
  * タプル：総理大臣の情報テーブルの中の各々のレコード。
  */
@@ -10,4 +13,30 @@ public class Tuple extends Object
 	 * まず、次のページを参照しながら、スケルトン（スタブ）を作ることから始めましょう。
 	 * http://www.cc.kyoto-su.ac.jp/~atsushi/Programs/CSV2HTML/PrimeMinistersJavaDoc/index.html
 	 */
+	//属性リストを記憶するフィールド。
+	private Attributes attributes = new Attributes();
+	//値リストを記憶するフィールド。
+	private ArrayList<String> value;
+	/*スタブ作成　10月22日　北川裕基
+	 * 属性リストと値リストからタプルを作るコンストラクタ。
+	 * */
+	Tuple(Attributes instanceOfAttributes, ArrayList<String> valueCollection){}
+	/*スタブ作成　10月22日　北川裕基
+	 * 属性リストを応答する。
+	 * */
+	public Attributes attributes(){
+		return null;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * 自分自身を文字列にして、それを応答する。
+	 * */
+	public String toString(){
+		return "";
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * 値リストを応答する。
+	 * */
+	public ArrayList<String> values(){
+		return null;
+	}
 }
