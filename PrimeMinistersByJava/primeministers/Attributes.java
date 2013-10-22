34c6ddab9ac4f2264ed9dc0cec07a7bcfad17d71
g1144407 <yuki@yuki-no-MacBook-Air.local>
2013-10-22 13:47:59 +0900

    Attributes スタブ作成

--- PrimeMinistersByJava/primeministers/Attributes.java
+++ PrimeMinistersByJava/primeministers/Attributes.java
@@ -1,5 +1,8 @@
 package primeministers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 属性群：総理大臣の情報テーブルを入出力する際の属性情報を記憶。
  */
@@ -10,4 +13,103 @@ public class Attributes extends Object
 	 * まず、次のページを参照しながら、スケルトン（スタブ）を作ることから始めましょう。
 	 * http://www.cc.kyoto-su.ac.jp/~atsushi/Programs/CSV2HTML/PrimeMinistersJavaDoc/index.html
 	 */
+	private List<String> keys = new ArrayList<>();
+	private List<String> names = new ArrayList<>();
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	Attributes(String aString){}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	protected String at(int index){
+		return "";
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	private int indexOf(String aString){
+		return 0;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public int indexOfDays(){
+		return 0;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public int indexOfKana(){
+		return 0;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public int indexOfName(){
+		return 0;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public int indexOfNo(){
+		return 0;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public int indexOfOrder(){
+		return 0;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public int indexOfParty(){
+		return 0;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public int indexOfPeriod(){
+		return 0;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public int indexPlace(){
+		return 0;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public int indexOfSchool(){
+		return 0;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public int indexOfThumbnail(){
+		return 0;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	protected String keyAt(int index){
+		return "";
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public ArrayList<String> keys(){
+		return null;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	protected String nameAt(int index){
+		return "";
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public ArrayList<String> names(){
+		return null;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public void names(ArrayList<String> aCollection){
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public int size(){
+		return 0;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public String toString(){
+		return "";
+	}
 }
