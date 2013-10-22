b46d090dad739681755fdb5a3d2ab7430adc9000
g1144407 <yuki@yuki-no-MacBook-Air.local>
2013-10-22 13:57:22 +0900

    Table スタブ作成

--- PrimeMinistersByJava/primeministers/Table.java
+++ PrimeMinistersByJava/primeministers/Table.java
@@ -1,5 +1,8 @@
 package primeministers;
 
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 /**
  * 表：総理大臣の情報テーブル。
  */
@@ -10,4 +13,49 @@ public class Table extends Object
 	 * まず、次のページを参照しながら、スケルトン（スタブ）を作ることから始めましょう。
 	 * http://www.cc.kyoto-su.ac.jp/~atsushi/Programs/CSV2HTML/PrimeMinistersJavaDoc/index.html
 	 */
+	private Attributes attribures = new Attributes();
+	private ArrayList<BufferedImage> images = new ArrayList<>();
+	private ArrayList<BufferedImage> thumbnails = new ArrayList<>();
+	private ArrayList<Tuple> tuples = new ArrayList<>();
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	Table(){}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public void add(Tuple tuple){
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public Attributes attributes(){
+		return null;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public void attributes(Attributes instanceOfAttributes){
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	ArrayList<BufferedImage> images(){
+		return null;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	private BufferedImage picture(String aString){
+		return null;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public ArrayList<BufferedImage> thumbnails(){
+		return null;
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public String toString(){
+		return "";
+	}
+	/*スタブ作成　10月22日　北川裕基
+	 * */
+	public ArrayList<Tuple> tuples(){
+		return null;
+	}
 }
