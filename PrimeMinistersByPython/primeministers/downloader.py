d57d786002a879d99d1144ec076916c7c86177a8
shogo <soleil_campus200@yahoo.co.jp>
2013-11-22 12:01:24 +0900

    filedo -python修正-
    pythonもデスクトップにディレクトリを作るようにした。

--- PrimeMinistersByPython/primeministers/downloader.py
+++ PrimeMinistersByPython/primeministers/downloader.py
@@ -25,7 +25,7 @@ class Downloader(io.IO):
 	def download_csv(self):
 		"""総理大臣の情報を記したCSVファイルをダウンロードする。"""
 		url = "http://www.cc.kyoto-su.ac.jp/~atsushi/Programs/CSV2HTML/PrimeMinisters/PrimeMinisters.csv"
-		dirpath = "PrimeMinisters/"
+		dirpath = os.environ['HOME']+"/Desktop/PrimeMinisters/"
 		
 		if not os.path.isdir(dirpath):
 			os.makedirs(dirpath)
@@ -38,16 +38,16 @@ class Downloader(io.IO):
 		"""画像ファイル群または縮小画像ファイル群をダウンロードする。"""
 		image_url = "http://www.cc.kyoto-su.ac.jp/~atsushi/Programs/CSV2HTML/PrimeMinisters/images/"+"0"+str(image_filenames)+".jpg"
 		thumbnail_url = "http://www.cc.kyoto-su.ac.jp/~atsushi/Programs/CSV2HTML/PrimeMinisters/thumbnails/"+"0"+str(image_filenames)+".jpg"
-		image_dirpath = "PrimeMinisters/images/"
-		thumbnail_dirpath = "PrimeMinisters/thumbnails/"
+		image_dirpath = os.environ['HOME']+"/Desktop/PrimeMinisters/images/"
+		thumbnail_dirpath = os.environ['HOME']+"/Desktop/PrimeMinisters/thumbnails/"
 		
 		if not os.path.isdir(image_dirpath):
 			os.makedirs(image_dirpath)
 		if not os.path.isdir(thumbnail_dirpath):
 			os.makedirs(thumbnail_dirpath)
 			
-		image_path = image_dirpath+"0"+image_filenames+".jpg"
-		thumbnail_path = thumbnail_dirpath+"0"+image_filenames+".jpg"	
+		image_path = image_dirpath+"0"+str(image_filenames)+".jpg"
+		thumbnail_path = thumbnail_dirpath+"0"+str(image_filenames)+".jpg"
 		urllib.urlretrieve(image_url,image_path)
 		urllib.urlretrieve(thumbnail_url,thumbnail_path)
 		return
